7789ffab0f23242aec68d9cb7efa15787a1ea38d
francis <clyderitchie@yahoo.com>
2024-10-30 20:33:13 -0700

    Trying a Deployment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
+
+declare global {
+    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
 // import { PrismaClient } from "@prisma/client";
 
 // const prismaClientSingleton = () => {
@@ -16,8 +32,8 @@
 
 // lib/prisma.ts
 
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export default prisma;
+// export default prisma;
