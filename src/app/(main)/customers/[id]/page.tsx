6689331d8f9494c269ecf38dcb7df29b2dd325e6
francis <clyderitchie@yahoo.com>
2024-11-09 19:02:05 -0800

    Customer profile page layout cleanup

--- src/app/(main)/customers/[id]/page.tsx
+++ src/app/(main)/customers/[id]/page.tsx
@@ -31,34 +31,41 @@ export default async function Page({ params: { id } }: PageProps) {
   }
 
   const customer = await getCustomer(id);
+  console.log(customer);
 
   return (
     <>
       <div className="h-screen w-full bg-card p-4 shadow-md">
         {customer ? (
           <div>
-            <div className="flex justify-between items-center rounded-md border bg-card px-2 py-3 text-start shadow-sm">
-              <h1 className="min-w-fit max-w-fit py-3 text-2xl">
+            <div className="flex items-center justify-between rounded-md border bg-card px-2 py-3 text-start shadow-sm">
+              <h1 className="min-w-fit max-w-fit py-3 text-3xl">
                 {customer.name}
               </h1>
               <ProfileExtra customerId={customer.id} />
             </div>
             {/*Customer Name Div */}
-            <div className="row-span-2 flex flex-col justify-around md:flex-row">
-              <div className="my-4 min-w-fit max-w-fit rounded-md border bg-card px-2 py-3 shadow-md">
-                <div className="my-2 flex flex-row items-baseline">
+            <div className="row-span-2 flex flex-col justify-around py-3 md:flex-row">
+              <div className="mx-1 my-4 w-1/2 max-w-full rounded-md border bg-card px-2 py-3 shadow-md">
+                <div className="my-2 flex flex-row items-baseline py-1">
                   <p className="pe-5 ps-2 text-lg font-semibold">Birthday: </p>
                   <p> {new Date(customer.birthday).toLocaleDateString()}</p>
                 </div>
-                <div className="my-2 flex flex-row items-baseline">
+                <div className="my-2 flex flex-row items-baseline py-1">
                   <p className="pe-5 ps-2 text-lg font-semibold">
                     Customer Since:{" "}
                   </p>
                   <p>{new Date(customer.createdAt).toLocaleDateString()}</p>
                 </div>
+                <div className="my-2 flex flex-row items-baseline py-1">
+                  <p className="pe-5 ps-2 text-lg font-semibold">
+                    Identification Type:{" "}
+                  </p>
+                  <p>{customer.identification}</p>
+                </div>
               </div>
               {/*Customer ID Info Div */}
-              <div className="my-4 min-w-fit max-w-fit rounded-md border bg-card px-2 py-3 shadow-md">
+              <div className="my-4 w-1/2 max-w-full rounded-md border bg-card px-2 py-3 shadow-md">
                 <h2 className="ps-2 text-lg font-bold">
                   Customer Information:{" "}
                 </h2>
@@ -84,12 +91,23 @@ export default async function Page({ params: { id } }: PageProps) {
                 {customer.name} Accounts:
               </h1>
               {customer.accounts.map((account) => (
-                <p key={account.id}>
-                  <span className="me-3">{account.accountNumber}</span>
-                  <span>{account.accountType}</span>
-                  <strong className="ms-4">Created At:</strong>{" "}
-                  {new Date(account.createdAt).toLocaleDateString()}
-                </p>
+                <div
+                  className="flex flex-row items-center justify-between"
+                  key={account.id}
+                >
+                  <div className="w-1/4 border py-5 px-3">
+                    <h3 className="mb-4">Account Type: </h3>
+                    <span>{account.accountType}</span>
+                  </div>
+                  <div className="w-1/3 border py-5 px-3">
+                    <h3 className="mb-4">Account Number: </h3>
+                    <span>{account.accountNumber}</span>
+                  </div>
+                  <div className="w-1/3 border py-5 px-3">
+                    <h3 className="mb-4">Created At: </h3>
+                    {new Date(account.createdAt).toLocaleDateString()}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
